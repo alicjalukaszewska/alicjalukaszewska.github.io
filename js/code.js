4b328c063ff8f5ec2c216fcdbb4282d28278bb9c
alicjalukaszewska <alicja.x.lukaszewska@gmail.com>
2018-01-10 01:09:57 +0100

    Header nearly done

--- js/code.js
+++ js/code.js
@@ -1,3 +1,14 @@
 (function (){
 
+function changeMenu() {
+	const menu = document.querySelector('.fixed-menu');
+	if (window.scrollY == 0){
+		menu.classList.remove('scrolled');
+	} else {
+		menu.classList.add('scrolled');
+	}
+}
+
+window.addEventListener('scroll', changeMenu);
+
 }());
