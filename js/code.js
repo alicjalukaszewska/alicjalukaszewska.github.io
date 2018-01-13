29a082da3318066a762b5e31e1618452c935c709
alicjalukaszewska <alicja.x.lukaszewska@gmail.com>
2018-01-13 20:45:55 +0100

    Added zoom function

--- js/code.js
+++ js/code.js
@@ -1,5 +1,6 @@
-(function (){
+// (function (){
 
+/* change menu background on scroll */
 function changeMenu() {
 	const menu = document.querySelector('.fixed-menu');
 	if (window.scrollY == 0){
@@ -11,4 +12,28 @@ function changeMenu() {
 
 window.addEventListener('scroll', changeMenu);
 
-}());
+
+/* show/hide zoomed project */
+
+const images = document.querySelectorAll('.image-wrapper');
+const closeBtn = document.querySelector('#close');
+const zoom = document.querySelector('#zoom');
+const zoomWrapper = document.querySelector('.zoomed-project');
+
+function zoomed (e) {
+	const zoomedImg = zoom.querySelector('img');
+	zoomedImg.src = this.firstElementChild.src;
+	zoom.style.display = "flex";
+}
+
+images.forEach(image => {
+	image.addEventListener('click', zoomed);
+})
+
+closeBtn.addEventListener('click', () => zoom.style.display = "none");
+
+// zoomWrapper.addEventListener('mouseleave', () => zoom.style.display = "none");
+
+
+
+// }());
