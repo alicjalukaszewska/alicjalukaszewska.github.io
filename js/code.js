855e4e15627809485e3689388f382a5e182d464a
alicjalukaszewska <alicja.x.lukaszewska@gmail.com>
2018-01-14 02:29:18 +0100

    First steps to change projects by arrows

--- js/code.js
+++ js/code.js
@@ -21,35 +21,53 @@ const zoomWindow = document.querySelector('#zoom');
 const zoomedProject = document.querySelector('.zoomed-project');
 const zoomedContent = document.querySelector('.zoomed-project .content');
 const zoomedImage = zoomedProject.querySelector('img');
+const backBtn = document.querySelector('#back');
+
+function hideZoom () {
+	backBtn.style.display = "none";
+	zoomedContent.classList.remove('visuallyhidden');
+	zoomedImage.addEventListener('click', zoomImage);
+}
 
 function zoomImage () {
-	zoomedContent.classList.toggle('visuallyhidden');
+	backBtn.style.display = "block";
+	zoomedContent.classList.add('visuallyhidden');
+	zoomedImage.removeEventListener('click', zoomImage);
+	zoomedImage.addEventListener('click', hideZoom);
 }
 
 function changeContent (e) {
+	console.log(e.target);
 	//reset content
 	zoomedContent.innerHTML = "";
-	zoomedContent.classList.remove('visuallyhidden');
+	hideZoom();
 	//change image
 	let image = this.querySelector('img').src;
-
 	zoomedImage.src = image;
-	
 	let content = this.querySelector('.content').innerHTML;
 	//set new content
 	zoomedContent.innerHTML = content;
 	//show window
 	zoomWindow.style.display = "flex";
+	nextBtn.addEventListener('click', () => findNextProject(this));
 }
 
 zoomedImage.addEventListener('click', zoomImage);
+backBtn.addEventListener('click', hideZoom);
 
 projects.forEach(project => {
 	project.addEventListener('click', changeContent);
 })
 
-
 closeBtn.addEventListener('click', () => zoom.style.display = "none");
 // window.addEventListener('click', () => zoom.style.display = "none");
+
+function findNextProject (e) {
+	changeContent(e.nextElementSibling);
+}
+
+const nextBtn = document.querySelector('#next');
+
+// nextBtn.addEventListener('click', nextProject);
 
 // }());
